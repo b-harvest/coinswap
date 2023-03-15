a3c140a16670a81b20bfe4442d07011a4e0e5f12
poorphd <bc.park@bharvest.io>
2023-03-15 11:42:22 +0900

    feat grpc query test

--- modules/coinswap/keeper/grpc_query_test.go
+++ modules/coinswap/keeper/grpc_query_test.go
@@ -11,3 +11,37 @@ func (s *TestSuite) TestGRPCParams() {
 	s.Require().NoError(err)
 	s.Require().Equal(s.keeper.GetParams(s.ctx), resp.Params)
 }
+
+func (s *TestSuite) TestGRPCPool() {
+	_, _ = createReservePool(s, denomBTC)
+	poolId := types.GetPoolId(denomBTC)
+	pool, _ := s.app.CoinswapKeeper.GetPool(s.ctx, poolId)
+
+	resp, err := s.queryClient.LiquidityPool(sdk.WrapSDKContext(s.ctx), &types.QueryLiquidityPoolRequest{LptDenom: pool.LptDenom})
+	s.Require().NoError(err)
+	s.Require().Equal(pool.Id, resp.Pool.Id)
+	s.Require().Equal(pool.EscrowAddress, resp.Pool.EscrowAddress)
+
+	balances, _ := s.app.CoinswapKeeper.GetPoolBalancesByLptDenom(s.ctx, pool.LptDenom)
+	liquidity := s.app.BankKeeper.GetSupply(s.ctx, pool.LptDenom)
+	params := s.app.CoinswapKeeper.GetParams(s.ctx)
+	actCoins := sdk.NewCoins(
+		resp.Pool.Standard,
+		resp.Pool.Token,
+	)
+	s.Equal(actCoins.Sort().String(), balances.Sort().String())
+	s.Equal(liquidity, resp.Pool.Lpt)
+	s.Equal(params.Fee.String(), resp.Pool.Fee)
+}
+
+func (s *TestSuite) TestGRPCPools() {
+	_, _ = createReservePool(s, denomBTC)
+	resp, err := s.queryClient.LiquidityPools(sdk.WrapSDKContext(s.ctx), &types.QueryLiquidityPoolsRequest{})
+	s.Require().NoError(err)
+	s.Require().Len(resp.Pools, 1)
+
+	_, _ = createReservePool(s, denomETH)
+	resp, err = s.queryClient.LiquidityPools(sdk.WrapSDKContext(s.ctx), &types.QueryLiquidityPoolsRequest{})
+	s.Require().NoError(err)
+	s.Require().Len(resp.Pools, 2)
+}
